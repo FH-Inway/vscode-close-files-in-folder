0c37bcae1c65590d0638f43f2814b70292b2fcfc
Florian Hopfner <florian.hopfner@inway.de>
2024-05-21 16:58:30 +0200

    add working prototype of files in folder closer

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,22 +10,17 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    let disposable = vscode.commands.registerCommand('close-files-in-folder.closeFilesInFolder', async function (folder) {
+			for (const document of vscode.workspace.textDocuments) {
+				console.log(document.uri.fsPath);
+				if (document.uri.fsPath.startsWith(folder.fsPath)) {
+						await vscode.window.showTextDocument(document, { preview: false });
+						await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+				}
+    	}
+		});
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "close-files-in-folder" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('close-files-in-folder.closeFilesInFolder', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from close-files-in-folder!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
